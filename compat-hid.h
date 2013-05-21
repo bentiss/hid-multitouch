237de593248e93d12d3564304349e2f28e949a47
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2013-05-21 18:57:39 +0200

    Add compat defines for 3.2
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- compat-hid.h
+++ compat-hid.h
@@ -103,10 +103,40 @@ int __compat___hid_register_driver(struct __compat_hid_driver *hdrv,
 
 void __compat_hid_unregister_driver(struct __compat_hid_driver *hdrv);
 
+#ifdef module_driver
+#undef module_driver
+#endif /* module_driver */
+
+/**
+ * module_driver() - Helper macro for drivers that don't do anything
+ * special in module init/exit. This eliminates a lot of boilerplate.
+ * Each module may only use this macro once, and calling it replaces
+ * module_init() and module_exit().
+ *
+ * @__driver: driver name
+ * @__register: register function for this driver type
+ * @__unregister: unregister function for this driver type
+ * @...: Additional arguments to be passed to __register and __unregister.
+ *
+ * Use this macro to construct bus specific macros for registering
+ * drivers, and do not use it on its own.
+ */
+#define module_driver(__driver, __register, __unregister, ...) \
+static int __init __driver##_init(void) \
+{ \
+	return __register(&(__driver) , ##__VA_ARGS__); \
+} \
+module_init(__driver##_init); \
+static void __exit __driver##_exit(void) \
+{ \
+	__unregister(&(__driver) , ##__VA_ARGS__); \
+} \
+module_exit(__driver##_exit);
 
 #ifdef module_hid_driver
 #undef module_hid_driver
 #endif /* module_hid_driver */
+
 /**
  * module_hid_driver() - Helper macro for registering a HID driver
  * @__hid_driver: hid_driver struct
