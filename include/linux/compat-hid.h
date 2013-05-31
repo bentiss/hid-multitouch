06279909a8a3e90ea59deb105c1b614edb7a8699
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2013-05-31 10:25:29 +0200

    Fix compile for kernel <3.9
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- include/linux/compat-hid.h
+++ include/linux/compat-hid.h
@@ -25,8 +25,40 @@
 #ifndef __COMPAT_HID_H
 #define __COMPAT_HID_H
 
+#include <linux/fs.h>
 #include <linux/version.h>
 #include <linux/backport.h>
+
+#ifndef IS_ENABLED
+/*
+ * Getting something that works in C and CPP for an arg that may or may
+ * not be defined is tricky.  Here, if we have "#define CONFIG_BOOGER 1"
+ * we match on the placeholder define, insert the "0," for arg1 and generate
+ * the triplet (0, 1, 0).  Then the last step cherry picks the 2nd arg (a one).
+ * When CONFIG_BOOGER is not defined, we generate a (... 1, 0) pair, and when
+ * the last step cherry picks the 2nd arg, we get a zero.
+ */
+#define __ARG_PLACEHOLDER_1 0,
+#define config_enabled(cfg) _config_enabled(cfg)
+#define _config_enabled(value) __config_enabled(__ARG_PLACEHOLDER_##value)
+#define __config_enabled(arg1_or_junk) ___config_enabled(arg1_or_junk 1, 0)
+#define ___config_enabled(__ignored, val, ...) val
+
+/*
+ * IS_ENABLED(CONFIG_FOO) evaluates to 1 if CONFIG_FOO is set to 'y' or 'm',
+ * 0 otherwise.
+ *
+ */
+#define IS_ENABLED(option) \
+	(config_enabled(option) || config_enabled(option##_MODULE))
+#endif /* IS_ENABLED */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 9, 0)
+static inline struct inode *file_inode(struct file *f)
+{
+	return f->f_path.dentry->d_inode;
+}
+#endif
 
 /**
  * general hid functions
