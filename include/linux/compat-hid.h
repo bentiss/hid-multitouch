6b8cbe71a30f20308ab22fe25e20ec9361a0d29d
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2013-12-18 16:21:30 -0500

    Add missing module_driver macro for some old kernels

--- include/linux/compat-hid.h
+++ include/linux/compat-hid.h
@@ -125,6 +125,34 @@ struct compat_hid_device_id {
 	compat_kernel_ulong_t driver_data;
 };
 
+#ifndef module_driver
+/**
+ * module_driver() - Helper macro for drivers that don't do anything
+ * special in module init/exit. This eliminates a lot of boilerplate.
+ * Each module may only use this macro once, and calling it replaces
+ * module_init() and module_exit().
+ *
+ * @__driver: driver name
+ * @__register: register function for this driver type
+ * @__unregister: unregister function for this driver type
+ * @...: Additional arguments to be passed to __register and __unregister.
+ *
+ * Use this macro to construct bus specific macros for registering
+ * drivers, and do not use it on its own.
+ */
+#define module_driver(__driver, __register, __unregister, ...) \
+static int __init __driver##_init(void) \
+{ \
+	return __register(&(__driver) , ##__VA_ARGS__); \
+} \
+module_init(__driver##_init); \
+static void __exit __driver##_exit(void) \
+{ \
+	__unregister(&(__driver) , ##__VA_ARGS__); \
+} \
+module_exit(__driver##_exit);
+#endif
+
 /**
  * general hid functions
  */
